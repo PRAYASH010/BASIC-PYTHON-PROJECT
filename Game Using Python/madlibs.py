326f84c332363bce7b6bef3004e47565d14ae376
PRAYASH <prayashadhikari537@gmail.com>
2026-06-30 16:30:01 +0545

    Fix project structure and add python games

---
+++ Game Using Python/madlibs.py
@@ -0,0 +1,7 @@
+APPLIANCE = input("Enter an appliance :")
+VERB = input("Enter a verb : ")
+TEXTURE = input("Enter the texture : ")
+PLURAL_NOUN = input("Enter a plural noun : ")
+EXCLAMATION = input("Enter an exclamation : ")
+
+print(f"Welcome back to Galactic Chef! Today, we were supposed to make a normal soufflé, but my trusted {APPLIANCE} had other plans.As soon as Iturned it on, the machine began to violently {VERB} across the counter. Suddenly, the lid flew off and a massive wave of {TEXTURE} {PLURAL_NOUN} exploded into the air, coating the entire studio audience!'{EXCLAMATION}!' screamed the judges as they ran for cover. Needless to say, I think I'm getting eliminated tonight.")
